031a58f8d340f526f38650ed84cc9f2ca256a7ef
于洋 <yuyang1@bogon>
2016-12-21 10:56:37 +0800

    Initial Commit

---
+++ First/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  First
+//
+//  Created by 于洋 on 16/12/21.
+//  Copyright © 2016年 于洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ First/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  First
+//
+//  Created by 于洋 on 16/12/21.
+//  Copyright © 2016年 于洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
